4c8cea1d95432d7852455ce6e165bd0ef552a86e
yagofoga7 <159278997+yagofoga7@users.noreply.github.com>
2024-09-25 15:32:39 -0300

    hjhk

---
+++ Cidade.java
@@ -0,0 +1,5 @@
+package atv2509;
+
+public class Cidade {
+
+}
---
+++ Endereco.java
@@ -0,0 +1,36 @@
+package atv2509;
+
+public class Endereco {
+	
+	private String rua;
+	private String bairro;
+	private String cep;
+	private String cidade;
+	private int numero;
+	public String getRua() {
+	return rua;
+	}
+	public void setRua (String rua) {
+	this.rua = rua;
+	}
+	public String getBairro() {
+	return bairro;
+	}
+	public void setBairro(String bairro) {
+	this.bairro = bairro;
+	}
+	public String getCidade() {
+	return cidade;
+	}
+	public void setCidade(String cidade) {
+	this.cidade = cidade;
+	}
+	public int getNumero() {
+	return numero;
+	}
+	public void setNumero(int numero) {
+	this.numero = numero;
+	}
+	}
+
+
